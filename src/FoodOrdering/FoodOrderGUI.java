434e9b27042a63cfb096117cafa485fa07afb92e
Jess Oliver Zoilo <zjessoliver@gmail.com>
2022-12-17 19:58:20 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -16,6 +18,46 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton r15;
     private JCheckBox cTea;
 
+    public FoodOrderGUI(){
+        super("Food Ordering System");
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                double price = 0;
+                if(cPizza.isSelected()){
+                    price += 100;
+                }
+                if(cBurger.isSelected()){
+                    price += 80;
+                }
+                if(cFries.isSelected()){
+                    price += 65;
+                }
+                if(cSoftDrinks.isSelected()){
+                    price += 55;
+                }
+                if(cTea.isSelected()){
+                    price += 50;
+                }
+                if(cSundae.isSelected()){
+                    price += 40;
+                }
+
+                if(r5.isSelected()){
+                    price *= 0.95;
+                }
+                if(r10.isSelected()){
+                    price *= 0.90;
+                }
+                if(r15.isSelected()){
+                    price *= 0.85;
+                }
+
+                JOptionPane.showMessageDialog(null, String.format("The total price is %.2f",price));
+            }
+        });
+    }
     public static void main(String[] args) {
         FoodOrderGUI app = new FoodOrderGUI();
         app.setContentPane(app.panel1);
